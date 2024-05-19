d6419bb43ca25a8fb2902cdf603a13b84a736030
soulfit15 <83112898+soulfit15@users.noreply.github.com>
2024-05-19 21:07:41 +0300

    Add files via upload

---
+++ FreshSelector/blue.cs
@@ -0,0 +1,32 @@
+using Godot;
+using System;
+
+public partial class blue : Sprite2D
+{
+	AnimationPlayer anim;
+	bool isSelected = false;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		var btn = GetNode<Button>("Button");
+		btn.Toggled += toggle;
+		anim = GetNode<AnimationPlayer>("AnimationPlayer");
+	}
+
+	private void toggle(bool toggledOn){
+		if (toggledOn)
+		{
+			anim.Play("anim");
+			isSelected = true;
+		} else if (!toggledOn)
+		{
+			anim.Stop();
+			isSelected = false;
+		}
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
